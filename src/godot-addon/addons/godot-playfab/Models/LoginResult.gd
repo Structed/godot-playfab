1648eb3d2dd3dc983b489bea04ad652ce31744ca
Johannes Ebner <Structed@users.noreply.github.com>
2022-01-28 23:30:25 +0100

    Added LoginResult model

---
+++ src/godot-addon/addons/godot-playfab/Models/LoginResult.gd
@@ -0,0 +1,26 @@
+extends Reference
+class_name LoginResult
+
+## If LoginTitlePlayerAccountEntity flag is set on the login request the title_player_account will also be logged in and returned.
+var EntityToken#: EntityTokenResponse
+
+# Results for requested info.
+var InfoResultPayload#: GetPlayerCombinedInfoResultPayload
+
+## The time of this user's previous login. If there was no previous login, then it's DateTime.MinValue
+var LastLoginTime: String
+
+## True if the account was newly created on this login.
+var NewlyCreated: bool
+
+## Player's unique PlayFabId.
+var PlayFabId: String
+
+## Unique token authorizing the user and game at the server level, for the current session.
+var SessionTicket: String
+
+## Settings specific to this user.
+var SettingsForUser#: UserSettings
+
+#The experimentation treatments for this user at the time of login.
+var TreatmentAssignment#: TreatmentAssignment
