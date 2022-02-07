c90cf0211870da85badc03021b934f263e9b7e5e
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-07 23:02:24 +0100

    Fixed splitting and general class generation

--- addons/godot-playfab/Scenes/PlayFabMainScreen.gd
+++ addons/godot-playfab/Scenes/PlayFabMainScreen.gd
@@ -1,66 +1,37 @@
-tool
+#tool
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_Generate_pressed():
-	print("test")
-#	var model = to_model($Input.text)
-#	$Output.text = model
-	
-	$Output.text = split_regex($Input.text)
+	var model = to_model($ClassNameContainer/LineEdit.text, $Input.text)
+	$Output.text = model
 
-static func to_model(input: String) -> String:
-	var lines = input.split("\\n", false)
+
+static func to_model(object_name: String, input: String) -> String:
+	var lines = input.split("\n", true)
 	
-	var model = ""
+	var props = []
+	var current_prop = ""
+	var prop_line = 0
 	for line in lines:
-		var str_line = (line as String)
-		print("\"%s\" has %d characters" % [str_line, str_line.length()])
+		
+		var str_line = (line as String).strip_edges()
+		
 		if not str_line.empty():
-			model += str_line
+			match prop_line:
+				0:
+					current_prop = str_line
+				1:
+					current_prop = "var %s: %s" % [current_prop, str_line] 
+				2:
+					current_prop = "# %s\n%s" % [str_line, current_prop]
+			
+			prop_line += 1
 		else:
-			pass
-			#print("\"%s\" has %d characters" % [str_line, str_line.length()])
-		print(line)
+			props.append(current_prop)
+			prop_line = 0
 		
+	var model = "extends Reference\nclass_name " + object_name + "\n\n"
+	for prop in props:
+		model += prop + "\n\n"
 	return model
-
-func split_regex(input: String):
-	var regex = RegEx.new()
-	regex.compile("(.+)\\s?$")
-	#regex.compile("^(.*)\\n(.*)\\n(.*)\n$")
-	
-	# ^(.*)\n(.*)\n(.*)\n$
-	
-	var result_string = ""
-	var index = 0
-	var results = []
-	var dict = {}
-	
-	
-	for result in regex.search_all(input):
-		var var_name = result.get_string(1) as String
-		var type = result.get_string(2) as String
-		var comment = result.get_string(3) as String
-		var formatted = "##%s\\nvar %s: %s" % [comment, var_name, type]
-		results.push_back(formatted)
-		print(formatted)
-
-	result_string = PoolStringArray(results).join("\\r\\n")
-	#print(results)
-		
-	return result_string
