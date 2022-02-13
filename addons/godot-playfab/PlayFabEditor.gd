801626f69b5139f779e9786601604a1de104d707
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-13 23:25:26 +0100

    Make scene a tool again!

--- addons/godot-playfab/PlayFabEditor.gd
+++ addons/godot-playfab/PlayFabEditor.gd
@@ -13,25 +13,25 @@ func _init() -> void:
 	if error: push_error("Encountered error %d when saving project settings." % error)
 
 
-#func _enter_tree():
-#	main_panel_instance = MainPanel.instance()
-#	# Add the main panel to the editor's main viewport.
-#	get_editor_interface().get_editor_viewport().add_child(main_panel_instance)
-#	# Hide the main panel. Very much required.
-#	make_visible(false)
-#
-#
-#func _exit_tree():
-#	if main_panel_instance:
-#		main_panel_instance.queue_free()
-#
-#func has_main_screen():
-#	return true
-#
-#
-#func make_visible(visible):
-#	if main_panel_instance:
-#		main_panel_instance.visible = visible
+func _enter_tree():
+	main_panel_instance = MainPanel.instance()
+	# Add the main panel to the editor's main viewport.
+	get_editor_interface().get_editor_viewport().add_child(main_panel_instance)
+	# Hide the main panel. Very much required.
+	make_visible(false)
+
+
+func _exit_tree():
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+func has_main_screen():
+	return true
+
+
+func make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
 
 
 func get_plugin_name():
