90c7037b82a89d8cdaa51bb90a79315f55bc8655
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-16 22:27:53 +0100

    Added models for login request

---
+++ addons/godot-playfab/Models/GetPlayerCombinedInfoRequestParams.gd
@@ -0,0 +1,62 @@
+extends Reference
+class_name GetPlayerCombinedInfoRequestParams
+
+# Whether to get character inventories. Defaults to false.
+var GetCharacterInventories: bool
+
+# Whether to get the list of characters. Defaults to false.
+var GetCharacterList: bool
+
+# Whether to get player profile. Defaults to false. Has no effect for a new player.
+var GetPlayerProfile: bool
+
+# Whether to get player statistics. Defaults to false.
+var GetPlayerStatistics: bool
+
+# Whether to get title data. Defaults to false.
+var GetTitleData: bool
+
+# Whether to get the player's account Info. Defaults to false
+var GetUserAccountInfo: bool
+
+# Whether to get the player's custom data. Defaults to false
+var GetUserData: bool
+
+# Whether to get the player's inventory. Defaults to false
+var GetUserInventory: bool
+
+# Whether to get the player's read only data. Defaults to false
+var GetUserReadOnlyData: bool
+
+# Whether to get the player's virtual currency balances. Defaults to false
+var GetUserVirtualCurrency: bool
+
+# Specific statistics to retrieve. Leave null to get all keys. Has no effect if GetPlayerStatistics is false
+var PlayerStatisticNames: Array
+
+# Specifies the properties to return from the player profile. Defaults to returning the player's display name.
+var ProfileConstraints: PlayerProfileViewConstraints
+
+# Specific keys to search for in the custom data. Leave null to get all keys. Has no effect if GetTitleData is false
+var TitleDataKeys: Array
+
+# Specific keys to search for in the custom data. Leave null to get all keys. Has no effect if GetUserData is false
+var UserDataKeys: Array
+
+# Specific keys to search for in the custom data. Leave null to get all keys. Has no effect if GetUserReadOnlyData is false
+var UserReadOnlyDataKeys: Array
+
+func show_all():
+	GetCharacterInventories = true
+	GetCharacterList = true
+	GetPlayerProfile = true
+	GetPlayerStatistics = true
+	GetTitleData = true
+	GetUserAccountInfo = true
+	GetUserData = true
+	GetUserInventory = true
+	GetUserReadOnlyData = true
+	GetUserVirtualCurrency = true
+	
+	ProfileConstraints = ProfileConstraints.new()
+	ProfileConstraints.show_all()
---
+++ addons/godot-playfab/Models/PlayerProfileViewConstraints.gd
@@ -0,0 +1,72 @@
+extends Reference
+class_name PlayerProfileViewConstraints
+
+# Whether to show player's avatar URL. Defaults to false
+var ShowAvatarUrl: bool
+
+# Whether to show the banned until time. Defaults to false
+var ShowBannedUntil: bool
+
+# Whether to show campaign attributions. Defaults to false
+var ShowCampaignAttributions: bool
+
+# Whether to show contact email addresses. Defaults to false
+var ShowContactEmailAddresses: bool
+
+# Whether to show the created date. Defaults to false
+var ShowCreated: bool
+
+# Whether to show the display name. Defaults to false
+var ShowDisplayName: bool
+
+# Whether to show player's experiment variants. Defaults to false
+var ShowExperimentVariants: bool
+
+# Whether to show the last login time. Defaults to false
+var ShowLastLogin: bool
+
+# Whether to show the linked accounts. Defaults to false
+var ShowLinkedAccounts: bool
+
+# Whether to show player's locations. Defaults to false
+var ShowLocations: bool
+
+# Whether to show player's membership information. Defaults to false
+var ShowMemberships: bool
+
+# Whether to show origination. Defaults to false
+var ShowOrigination: bool
+
+# Whether to show push notification registrations. Defaults to false
+var ShowPushNotificationRegistrations: bool
+
+# Reserved for future development
+var ShowStatistics: bool
+
+# Whether to show tags. Defaults to false
+var ShowTags: bool
+
+# Whether to show the total value to date in usd. Defaults to false
+var ShowTotalValueToDateInUsd: bool
+
+# Whether to show the values to date. Defaults to false
+var ShowValuesToDate: bool
+
+func show_all():
+	ShowAvatarUrl = true
+	ShowBannedUntil = true
+	ShowCampaignAttributions = true
+	ShowContactEmailAddresses = true
+	ShowCreated = true
+	ShowDisplayName = true
+	ShowExperimentVariants = true
+	ShowLastLogin = true
+	ShowLinkedAccounts = true
+	ShowLocations = true
+	ShowMemberships = true
+	ShowOrigination = true
+	ShowPushNotificationRegistrations = true
+	ShowStatistics = true
+	ShowTags = true
+	ShowTotalValueToDateInUsd = true
+	ShowValuesToDate = true
