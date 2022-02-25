7516a14ee8acae9097503817d7ccaa6d0c6e858e
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-25 23:29:16 +0100

    Create PlayFabConfig.gd

---
+++ addons/godot-playfab/PlayFabConfig.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name PlayFabConfig, "res://addons/godot-playfab/icon.png"
+
+
+export(String) var title_id
+export(String) var base_uri = "playfabapi.com"
+export(String) var session_ticket = ""
