e41cd9ad4693198c6854beab684dc054a9d8b017
Johannes Ebner <Structed@users.noreply.github.com>
2022-02-27 01:30:48 +0100

    Full first implementation of PlayFabConfig

--- addons/godot-playfab/PlayFabConfig.gd
+++ addons/godot-playfab/PlayFabConfig.gd
@@ -2,6 +2,39 @@ extends Resource
 class_name PlayFabConfig, "res://addons/godot-playfab/icon.png"
 
 
-export(String) var title_id
+const CONFIG_LOAD_PATH = "user://playfab_config.tres"
+
+
+export(String) var title_id setget set_title_id
 export(String) var base_uri = "playfabapi.com"
-export(String) var session_ticket = ""
+export(String) var api_url
+export(String) var session_ticket = "" setget set_session_ticket,get_session_ticket
+
+
+func set_title_id(value: String):
+	api_url = "https://%s.%s" % [ value, base_uri ]
+
+
+func set_session_ticket(value: String):
+	session_ticket = value
+	
+
+func get_session_ticket() -> String:
+	guard_session_ticket_not_null_or_empty()
+	return session_ticket
+
+
+func is_logged_in() -> bool:
+	return !session_ticket.empty()
+
+
+func is_not_logged_in() -> bool:
+	return session_ticket.empty()
+	
+
+func guard_session_ticket_not_null_or_empty() -> bool:
+	if session_ticket == null || session_ticket == "":
+		push_error("Session ticket is empty.")
+		return false
+	
+	return true
