a14e17e2b4d2acace71ce35ec6fa24d8a8d28219
Johannes Ebner <Structed@users.noreply.github.com>
2022-03-26 22:23:31 +0100

    Update code docs

--- addons/godot-playfab/Models/AbstractJsonSerializableCollection.gd
+++ addons/godot-playfab/Models/AbstractJsonSerializableCollection.gd
@@ -1,22 +1,27 @@
+# This is a wrapper class around an Array
 extends JsonSerializable
 class_name AbstractJsonSerializableCollection
 
 
+# Holds the items
 var _Items: Array
 
 
+# Appends an element at the end of the array (alias of push_back).
 func append(item: JsonSerializable):
 	_Items.append(item)
 
 
+# Returns the number of elements in the array.
 func size() -> int:
 	return _Items.size()
 
-
+# Clears the array. This is equivalent to using resize with a size of 0.
 func clear():
 	_Items.clear();
 
 
+# Casts this collection and sub.objects to a Dictionary, recursively
 func to_dict() -> Dictionary:
 	var index = 0
 	var dict = []
@@ -26,6 +31,8 @@ func to_dict() -> Dictionary:
 
 	return dict
 
+
+# Rehydrates a collection and appropriate sub-objects from a Dictionary, recursively
 func from_dict(data: Dictionary, instance: JsonSerializable):
 	var index = 0
 	for item in data:
