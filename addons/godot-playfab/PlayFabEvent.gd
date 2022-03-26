aa2eb560e072b6fc2964fc895cc0e088caaee4d1
Johannes Ebner <Structed@users.noreply.github.com>
2022-03-26 22:07:43 +0100

    Updated documentation

--- addons/godot-playfab/PlayFabEvent.gd
+++ addons/godot-playfab/PlayFabEvent.gd
@@ -3,16 +3,18 @@ class_name PlayFabEvent, "res://addons/godot-playfab/icon.png"
 
 
 
-## Emitted when the PlayStream Event batch was flushed
-## @param event_ids: The IDs of the events written
+# Emitted when the PlayStream Event batch was flushed
+# @param event_ids: The IDs of the events written
 signal event_batch_playstream_flushed(event_ids)
 
-## Emitted when the Telemetry Event batch was flushed
-## @param event_ids: The IDs of the events written
+# Emitted when the Telemetry Event batch was flushed
+# @param event_ids: The IDs of the events written
 signal event_batch_telemetry_flushed(event_ids)
 
-
+# Defines the event batch size threshold
 export(int) var event_batch_size = 5
+
+# Defines the wait time (in seconds) until batches are flushed
 export(int) var event_batch_timeout_seconds = 10
 
 
@@ -31,20 +33,22 @@ func _ready():
 
 
 func _process(_delta):
-	check_event_batch_sizes()
+	_flush_batches_on_batch_size_met()
 
 
+# Write batches of entity based events to as Telemetry events (bypass PlayStream). The namespace must be 'custom' or start with 'custom.'
 func event_telemetry_write_events(request_data: WriteEventsRequest, callback: FuncRef):
 	_post_with_entity_auth(request_data, "/Event/WriteTelemetryEvents", callback)
 
 
+# Write batches of entity based events to PlayStream. The namespace of the Event must be 'custom' or start with 'custom.'.
 func event_playstream_write_events(request_data: WriteEventsRequest, callback: FuncRef):
 	_post_with_entity_auth(request_data, "/Event/WriteEvents", callback)
 
 
 # Directly write a Telemetry Event
 func write_title_player_telemetry_event(event_name: String, payload: Dictionary, callback: FuncRef, event_namespace = "custom.%s" % _title_id):
-	var event = assemble_event(event_name, payload, event_namespace)
+	var event = _assemble_event(event_name, payload, event_namespace)
 	# We send a batch of events here!
 	var request = WriteEventsRequest.new()
 	request.Events.append(event)
@@ -53,24 +57,27 @@ func write_title_player_telemetry_event(event_name: String, payload: Dictionary,
 
 # Directly write a PlayStream Event
 func write_title_player_playstream_event(event_name: String, payload: Dictionary, callback: FuncRef, event_namespace = "custom.%s" % _title_id):
-	var event = assemble_event(event_name, payload, event_namespace)
+	var event = _assemble_event(event_name, payload, event_namespace)
 	# We send a batch of events here!
 	var request = WriteEventsRequest.new()
 	request.Events.append(event)
 	event_playstream_write_events(request, callback)
 
 
+# Batch a Telemetry Event for later sending
 func batch_title_player_telemetry_event(event_name: String, payload: Dictionary, callback: FuncRef, event_namespace = "custom.%s" % _title_id):
-	var event = assemble_event(event_name, payload, event_namespace)
+	var event = _assemble_event(event_name, payload, event_namespace)
 	telemetry_event_batch.append(event)
 
 
+# Batch a PlayStream Event for later sending
 func batch_title_player_playstream_event(event_name: String, payload: Dictionary, callback: FuncRef, event_namespace = "custom.%s" % _title_id):
-	var event = assemble_event(event_name, payload, event_namespace)
+	var event = _assemble_event(event_name, payload, event_namespace)
 	playstream_event_batch.append(event)
 
-
-func assemble_event(event_name: String, payload: Dictionary, callback: FuncRef, event_namespace = "custom.%s" % _title_id) -> EventContents:
+# Assembles event data
+# @Visivbility: Private
+func _assemble_event(event_name: String, payload: Dictionary, callback: FuncRef, event_namespace = "custom.%s" % _title_id) -> EventContents:
 	var event = EventContents.new()
 	event.Name = event_name
 	event.EventNamespace = event_namespace
@@ -82,28 +89,16 @@ func assemble_event(event_name: String, payload: Dictionary, callback: FuncRef, 
 	return event
 
 
-## Batch a PlayStream Event as master_player_account
-## WILL NOT WORK IN A REGULAR CLIENT! As you only have a title_player_account token
-func batch_master_player_playstream_event(event_name: String, payload: Dictionary, callback: FuncRef, event_namespace = "custom.%s" % _title_id):
-	var event = EventContents.new()
-	event.Name = event_name
-	event.EventNamespace = event_namespace
-	event.Payload = payload
-	var entity = EntityKey.new()
-	if _playfab_client_config.is_logged_in(): # Needed, also loads the config. If not logged in, willl use the current Entity
-		entity.Id = _playfab_client_config.master_player_account_id
-		entity.Type = EntityKey.ENTITY_TYPE_MASTER_PLAYER_ACCOUNT
-		event.Entity = entity
-	playstream_event_batch.append(event)
-
-
-func check_event_batch_sizes():
+# Tiggers batch flush if comfigured treshold is met
+# @Visivbility: Private
+func _flush_batches_on_batch_size_met():
 	if playstream_event_batch.size() >= event_batch_size:
 		_flush_playstream_event_batch()
 	elif telemetry_event_batch.size() >= event_batch_size:
 		_flush_telemetry_event_batch()
 
-
+# Flushes the PlayStream event batch
+# @Visivbility: Private
 func _flush_playstream_event_batch():
 	if playstream_event_batch.size() < 1:
 		print_debug("No telemetry events to flush")
@@ -115,6 +110,9 @@ func _flush_playstream_event_batch():
 	playstream_event_batch.clear()
 	print_debug("Flushed playstream batch")
 
+
+# Flushes the Telemetry event batch
+# @Visivbility: Private
 func _flush_telemetry_event_batch():
 	if telemetry_event_batch.size() < 1:
 		print_debug("No playstream events to flush")
@@ -127,12 +125,16 @@ func _flush_telemetry_event_batch():
 	print_debug("Flushed telemetry batch")
 
 
+# Callback for after PlayStream batch flush
+# @Visivbility: Private
 func _on_playstream_batch_flush(response: Dictionary):
 	var event_ids: Array = response["data"]["AssignedEventIds"]
 	print_debug("Flushed %s PlayStream events" % event_ids.size())
 	emit_signal("event_batch_playstream_flushed", event_ids)
 
 
+# Callback for after Telemetry batch flush
+# @Visivbility: Private
 func _on_telemetry_batch_flush(response: Dictionary):
 	var event_ids: Array = response["data"]["AssignedEventIds"]
 	print_debug("Flushed %s Telemetry events" % event_ids.size())
