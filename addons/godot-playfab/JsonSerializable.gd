f767a6e4e60e55db7e60cb588c17dabf01910e75
Johannes Ebner <Structed@users.noreply.github.com>
2022-03-26 21:47:23 +0100

    Removed commented-out code

--- addons/godot-playfab/JsonSerializable.gd
+++ addons/godot-playfab/JsonSerializable.gd
@@ -7,16 +7,16 @@ func _get_type_for_property(property_name: String):
 	return ""
 
 func to_dict() -> Dictionary:
-	
+
 	var dict = {}
 	var props = get_property_list()
-	
+
 	# Skipping the first 3 items because they are metadata we do not need
 	for i in range(3, props.size()):
 		var prop = props[i]
 		var name = prop["name"] # The name of the property on the object. WIll be used to access its's value
 		var type = prop["type"]	# The godot built-in type (Array, Object etc)
-		
+
 		if (type == TYPE_OBJECT):
 			# Get the value of the property
 			var sub_prop = get(name)
@@ -30,28 +30,23 @@ func to_dict() -> Dictionary:
 				# No to_dict method - likely an error!
 				print_debug("if this is not a native object, pelase implement a to_dict method!")
 				dict[name] = sub_prop
-#		elif type == TYPE_ARRAY:
-#			var sub_dict = []
-#			for element in prop:
-#				sub_dict.append(element)
-#			dict[name] = sub_dict
 		else:
 			# Get the value of the property
 			dict[name] = get(name)
-	
+
 	return dict
 
 func from_dict(data: Dictionary, instance: JsonSerializable):
-	
+
 	var props = instance.get_property_list()
 	for key in data.keys():
-		
-		var type		
+
+		var type
 		for prop in props:
 			if prop["name"] == key:
 				type = prop["type"]
 				break
-		
+
 		# If basic data type - just set it
 		if type != TYPE_OBJECT:
 			instance.set(key, data[key])
@@ -69,7 +64,7 @@ func get_class_instance(name: String) -> Reference:
 	for element in classes:
 		if element["class"] == name:
 			return load(element["path"]).new()
-	
-	
+
+
 	push_error("Class \"" + name + "\" could not be found")
 	return null
