fe5106526253358dea7da14c9e3c5f54374db18a
Johannes Ebner <Structed@users.noreply.github.com>
2022-03-28 23:37:33 +0200

    Added mroe tests!

---
+++ addons/godot-playfab/test/unit/JsonSerializableImpl.gd
@@ -0,0 +1,5 @@
+extends JsonSerializable
+class_name JsonSerializableImpl
+
+var foo: String = "foo_value"
+var bar: String = "bar_value"
--- addons/godot-playfab/test/unit/test_JsonSerializable.gd
+++ addons/godot-playfab/test/unit/test_JsonSerializable.gd
@@ -4,7 +4,7 @@ var model
 
 func before_each():
 	gut.p("ran setup", 2)
-	model = JsonSerializable.new()
+	model = JsonSerializableImpl.new()
 
 func after_each():
 	gut.p("ran teardown", 2)
@@ -16,6 +16,20 @@ func after_all():
 	gut.p("ran run teardown", 2)
 
 
+func test_implements_JsonSerializable():
+	assert_is(model, JsonSerializable);
+
+
 func test_to_dict_returns_dictionary():
 	var dict = model.to_dict();
 	assert_true(dict is Dictionary)
+
+func test_to_dict_returns_expected_dictionary():
+	var actual = model.to_dict();
+	var expected = {
+		"foo": "foo_value",
+		"bar": "bar_value"
+	}
+
+	var result = compare_deep(expected, actual)
+	assert_true(result.are_equal)
