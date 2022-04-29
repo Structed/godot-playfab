ece97e2b9903142b497f721ef859da1c3e294736
Johannes Ebner <Structed@users.noreply.github.com>
2022-04-29 15:50:19 +0200

    Do not display LoginResult data if there is no Loginresutl set

--- Scenes/LoggedIn.gd
+++ Scenes/LoggedIn.gd
@@ -10,13 +10,16 @@ func _ready():
 
 # Called when the node enters the scene tree for the first time.
 func update():
-	$VBoxContainer/AccountPlayerId/Edit.text = login_result.PlayFabId
-	$VBoxContainer/TitlePlayerId/Edit.text = login_result.InfoResultPayload.AccountInfo.TitleInfo.TitlePlayerAccount.Id
-	$VBoxContainer/TitlePlayerName/Edit.text = login_result.InfoResultPayload.AccountInfo.TitleInfo.DisplayName
-	$VBoxContainer/SessionTicket/Edit.text = login_result.SessionTicket
-	$VBoxContainer/EntityToken/Edit.text = login_result.EntityToken.EntityToken
-	$VBoxContainer/EntityType/Edit.text = login_result.EntityToken.Entity.Type
-	$VBoxContainer/EntityId/Edit.text = login_result.EntityToken.Entity.Id
+	if login_result != null:
+		$VBoxContainer/LoginResultContainer/AccountPlayerId/Edit.text = login_result.PlayFabId
+		$VBoxContainer/LoginResultContainer/TitlePlayerId/Edit.text = login_result.InfoResultPayload.AccountInfo.TitleInfo.TitlePlayerAccount.Id
+		$VBoxContainer/LoginResultContainer/TitlePlayerName/Edit.text = login_result.InfoResultPayload.AccountInfo.TitleInfo.DisplayName
+		$VBoxContainer/LoginResultContainer/SessionTicket/Edit.text = login_result.SessionTicket
+		$VBoxContainer/LoginResultContainer/EntityToken/Edit.text = login_result.EntityToken.EntityToken
+		$VBoxContainer/LoginResultContainer/EntityType/Edit.text = login_result.EntityToken.Entity.Type
+		$VBoxContainer/LoginResultContainer/EntityId/Edit.text = login_result.EntityToken.Entity.Id
+
+		$VBoxContainer/LoginResultContainer.show()
 
 
 func _on_Button_pressed():
