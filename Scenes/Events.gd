be4f2f4f03fcdec5d71d5a5e01e3694229c13336
Johannes Ebner <Structed@users.noreply.github.com>
2022-04-29 15:33:42 +0200

    Moved Event API code to new Events scene

---
+++ Scenes/Events.gd
@@ -0,0 +1,53 @@
+extends Node
+
+
+const EVENT_NAME_TELEMETRY = "title_player_telemetry_event"
+const EVENT_NAME_PLAYSTREAM = "title_player_playstream_event"
+
+func _ready():
+	var _error = PlayFabManager.client.connect("api_error", self, "_on_PlayFab_api_error")
+	_error = PlayFabManager.event.connect("api_error", self, "_on_PlayFab_api_error")
+
+
+func _on_BatchTelemetryEventsButton_pressed():
+	var payload = {
+		"Action": "_on_BatchTelemetryEventsButton_pressed"
+	}
+
+	PlayFabManager.event.batch_title_player_telemetry_event(EVENT_NAME_TELEMETRY, payload, funcref(self, "_on_write_events_request_completed"))
+
+
+func _on_BatchPlayStreamEventsButton_pressed():
+	var payload = {
+		"Action": "_on_BatchPlayStreamEventsButton_pressed"
+	}
+
+	PlayFabManager.event.batch_title_player_playstream_event(EVENT_NAME_PLAYSTREAM, payload, funcref(self, "_on_write_events_request_completed"))
+
+
+func _on_WriteTelemetryDirectButton_pressed():
+	var payload = {
+		"Action": "_on_WriteTelemetryDirectButton_pressed"
+	}
+
+	PlayFabManager.event.write_title_player_telemetry_event(EVENT_NAME_TELEMETRY, payload, funcref(self, "_on_write_events_request_completed"))
+
+
+func _on_WritePlayStreamDirectButton_pressed():
+	var payload = {
+		"Action": "_on_WritePlayStreamDirectButton_pressed"
+	}
+
+	PlayFabManager.event.write_title_player_playstream_event(EVENT_NAME_PLAYSTREAM, payload, funcref(self, "_on_write_events_request_completed"))
+
+
+func _on_write_events_request_completed(response):
+	$VBoxContainer/Response.text = JSON.print(response.data, "\t")
+
+
+func _on_PlayFab_api_error(error: ApiErrorWrapper):
+	print_debug(error.errorMessage)
+
+
+func _on_BackButton_pressed():
+	SceneManager.goto_scene("res://Scenes/LoggedIn.tscn")
--- Scenes/LoggedIn.gd
+++ Scenes/LoggedIn.gd
@@ -6,7 +6,6 @@ var login_result: LoginResult
 
 func _ready():
 	var _error = PlayFabManager.client.connect("api_error", self, "_on_PlayFab_api_error")
-	_error = PlayFabManager.event.connect("api_error", self, "_on_PlayFab_api_error")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,44 +29,9 @@ func _on_get_title_data(response):
 	$VBoxContainer/Response.text = JSON.print(response.data, "\t")
 
 
-const EVENT_NAME_TELEMETRY = "title_player_telemetry_event"
-const EVENT_NAME_PLAYSTREAM = "title_player_playstream_event"
-
-func _on_BatchTelemetryEventsButton_pressed():
-	var payload = {
-		"Action": "_on_BatchTelemetryEventsButton_pressed"
-	}
-
-	PlayFabManager.event.batch_title_player_telemetry_event(EVENT_NAME_TELEMETRY, payload, funcref(self, "_on_write_events_request_completed"))
-
-
-func _on_BatchPlayStreamEventsButton_pressed():
-	var payload = {
-		"Action": "_on_BatchPlayStreamEventsButton_pressed"
-	}
-
-	PlayFabManager.event.batch_title_player_playstream_event(EVENT_NAME_PLAYSTREAM, payload, funcref(self, "_on_write_events_request_completed"))
-
-
-func _on_write_events_request_completed(response):
-	$VBoxContainer/Response.text = JSON.print(response.data, "\t")
-
-
-func _on_WriteTelemetryDirectButton_pressed():
-	var payload = {
-		"Action": "_on_WriteTelemetryDirectButton_pressed"
-	}
-
-	PlayFabManager.event.write_title_player_telemetry_event(EVENT_NAME_TELEMETRY, payload, funcref(self, "_on_write_events_request_completed"))
-
-
-func _on_WritePlayStreamDirectButton_pressed():
-	var payload = {
-		"Action": "_on_WritePlayStreamDirectButton_pressed"
-	}
-
-	PlayFabManager.event.write_title_player_playstream_event(EVENT_NAME_PLAYSTREAM, payload, funcref(self, "_on_write_events_request_completed"))
-
-
 func _on_PlayFab_api_error(error: ApiErrorWrapper):
 	print_debug(error.errorMessage)
+
+
+func _on_EventsPlayStream_pressed():
+	SceneManager.goto_scene("res://Scenes/Events.tscn")
