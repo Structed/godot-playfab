e9dd38e53a3101558547d1da30fe750c06c5022b
Johannes Ebner <Structed@users.noreply.github.com>
2022-06-10 14:25:55 +0200

    Implemented updating player statistics

--- addons/godot-playfab/PlayFabClient.gd
+++ addons/godot-playfab/PlayFabClient.gd
@@ -6,3 +6,7 @@ class_name PlayFabClient, "res://addons/godot-playfab/icon.png"
 # @param callback: FuncRef
 func get_title_data(request_data: GetTitleDataRequest, callback: FuncRef):
 	_post_with_session_auth(request_data, "/Client/GetTitleData", callback)
+
+
+func update_player_statistic(request_data: UpdatePlayerStatisticsRequest, callback: FuncRef):
+	_post_with_session_auth(request_data, "/Client/UpdatePlayerStatistics", callback)
