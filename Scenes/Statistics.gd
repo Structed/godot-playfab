ee0476f068ac8ebd5a1a626fab02d897ed64ffd0
Johannes Ebner <Structed@users.noreply.github.com>
2022-06-10 14:25:55 +0200

    Implemented updating player statistics

--- Scenes/Statistics.gd
+++ Scenes/Statistics.gd
@@ -7,6 +7,7 @@ var waiting = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start()
+	var _error = PlayFabManager.client.connect("api_error", self, "_on_PlayFab_api_error")
 
 
 func _process(_delta):
@@ -18,6 +19,7 @@ func _on_StopWaitingButton_pressed():
 	if (waiting):
 		waiting = false
 		$StopWaitingButton.text = "Start waiting for Godot!"
+		_update_statistic(get_elapsed_time())
 	else:
 		start()
 
@@ -30,3 +32,23 @@ func start():
 	start_time = OS.get_unix_time()
 	waiting = true
 	$StopWaitingButton.text = "Stop waiting for Godot!"
+
+
+func _update_statistic(value: int):
+	var request_data = UpdatePlayerStatisticsRequest.new()
+	var statistic = StatisticUpdate.new()
+
+	statistic.StatisticName = "time_waiting"
+	statistic.Value = value
+	statistic.Version = 1	# Make sure to imncrement this for updates where rules changes the scores
+
+	request_data.Statistics = StatisticUpdateCollection.new()
+	request_data.Statistics.append(statistic)
+	PlayFabManager.client.update_player_statistic(request_data, funcref(self, "_on_update_statistics_request_completed"))
+
+func _on_update_statistics_request_completed(_result):
+	print_debug("Completed sending stats")
+
+
+func _on_PlayFab_api_error(error: ApiErrorWrapper):
+	print_debug(error.errorMessage)
