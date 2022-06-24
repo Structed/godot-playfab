c09c525d6098679cd23c098a9ccf950a06c0ccbe
Johannes Ebner <Structed@users.noreply.github.com>
2022-06-24 15:43:32 +0200

    Added Back button in RequestBuilder scene

--- Scenes/RequestBuilder.gd
+++ Scenes/RequestBuilder.gd
@@ -7,3 +7,7 @@ var key_value_line_edit_scene = preload("res://Scenes/Widgets/KeyValueLineEdit.t
 func _on_AddLineButton_pressed():
 	var kvl = key_value_line_edit_scene.instance()
 	$TableContainer.add_child(kvl)
+
+func _on_BackButton_pressed():
+	SceneManager.goto_scene("res://Scenes/LoggedIn.tscn")
+
