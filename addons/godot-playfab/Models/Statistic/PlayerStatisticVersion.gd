ca27e43cc9873525b58cecab81f87659af19dc9c
Johannes Ebner <Structed@users.noreply.github.com>
2022-07-08 14:45:35 +0200

    Added GetPlayerStatisticVersions API

---
+++ addons/godot-playfab/Models/Statistic/PlayerStatisticVersion.gd
@@ -0,0 +1,32 @@
+extends JsonSerializable
+class_name PlayerStatisticVersion
+
+# time when the statistic version became active
+var ActivationTime: String
+
+# time when the statistic version became inactive due to statistic version incrementing
+var DeactivationTime: String
+
+# time at which the statistic version was scheduled to become active, based on the configured ResetInterval
+var ScheduledActivationTime: String
+
+# time at which the statistic version was scheduled to become inactive, based on the configured ResetInterval
+var ScheduledDeactivationTime: String
+
+# name of the statistic when the version became active
+var StatisticName: String
+
+# version of the statistic
+var Version: float
+
+
+func _get_type_for_property(property_name: String) -> String:
+	match property_name:
+#		"<PROPERTY NAME>":
+#			return "<PROPERTY TYPE>"
+		_:
+			pass
+
+	push_error("Could not find mapping for property: " + property_name)
+	return ._get_type_for_property(property_name)
+
---
+++ addons/godot-playfab/Models/Statistic/PlayerStatisticVersionCollection.gd
@@ -0,0 +1,5 @@
+extends AbstractJsonSerializableCollection
+class_name PlayerStatisticVersionCollection
+
+func _init():
+	_item_type = PlayerStatisticVersion
