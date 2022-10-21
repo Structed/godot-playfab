6366d147be18c0e84ddd9630891a30e246781d25
Johannes Ebner <Structed@users.noreply.github.com>
2022-10-22 00:41:05 +0200

    Made DateTimeHelper methods static

--- addons/godot-playfab/test/unit/Helper/test_DateTimeHelper.gd
+++ addons/godot-playfab/test/unit/Helper/test_DateTimeHelper.gd
@@ -1,14 +1,11 @@
 extends "res://addons/gut/test.gd"
 
 func test_to_date_time_returns_string():
-	var date_time_helper = DateTimeHelper.new()
-	var datetime_string = date_time_helper.to_date_time(0, 123)
+	var datetime_string = DateTimeHelper.to_date_time(0, 123)
 	assert_typeof(datetime_string, TYPE_STRING)
 
 func test_to_date_time_returns_zero_padded_elements():
-	var date_time_helper = DateTimeHelper.new()
-	
-	var datetime_string = date_time_helper.to_date_time(0, 123)
+	var datetime_string = DateTimeHelper.to_date_time(0, 123)
 	var expected = "1970-01-01T00:00:00.123Z"
 	assert_eq(datetime_string, expected)
 
@@ -19,8 +16,6 @@ var different_params = [
 	[3600, "1970-01-01T01:00:00.123Z"]
 ]
 func test_to_date_time_returns_different_timestamps(params=use_parameters(different_params)):
-	var date_time_helper = DateTimeHelper.new()
-	
-	var datetime_string = date_time_helper.to_date_time(params[0], 123)
+	var datetime_string = DateTimeHelper.to_date_time(params[0], 123)
 	var expected = "1970-01-01T00:00:00.123Z"
 	assert_eq(datetime_string, params[1])
