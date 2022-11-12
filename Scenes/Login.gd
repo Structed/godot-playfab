b77e844ed7bf518131e9f1adac3da686214a8557
zymsbgt <91821082+zymsbgt@users.noreply.github.com>
2022-11-12 15:37:08 +0800

    Removed changes to UI and mono

--- Scenes/Login.gd
+++ Scenes/Login.gd
@@ -1,6 +1,5 @@
 extends MarginContainer
 
-var isAnonLogin
 
 func _ready():
 	var _error = PlayFabManager.client.connect("api_error", self, "_on_api_error")
@@ -10,11 +9,10 @@ func _ready():
 func _on_Login_pressed():
 	$Login.hide()
 	_show_progess()
-	
-	isAnonLogin = false
+
 	var email = $Login/Email/Input.text
 	var password = $Login/Password/Input.text
-	
+
 	var combined_info_request_params = GetPlayerCombinedInfoRequestParams.new()
 	combined_info_request_params.show_all()
 	var player_profile_view_constraints = PlayerProfileViewConstraints.new()
@@ -24,7 +22,6 @@ func _on_Login_pressed():
 
 
 func _on_AnonLogin_pressed():
-	isAnonLogin = true
 	$Login.hide()
 	_show_progess()
 	var combined_info_request_params = GetPlayerCombinedInfoRequestParams.new()
@@ -46,10 +43,7 @@ func _hide_progess():
 
 
 func _on_logged_in(login_result: LoginResult):
-	if isAnonLogin == false:
-		$Login/Login.self_modulate = Color(0, 1, 0, 0.5)
-	else:
-		$Login/AnonLogin.self_modulate = Color(0, 1, 0, 0.5)
+	$Login/Login.self_modulate = Color(0, 1, 0, 0.5)
 	$Login/Output.hide()
 	_hide_progess()
 
