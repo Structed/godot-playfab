248972cdaf9891e5fd8a3308471831967ee00c9c
Johannes Ebner <Structed@users.noreply.github.com>
2022-12-18 13:20:07 +0100

    feat(pencil): Add response compression (gzip) (#87)

--- addons/godot-playfab/PlayFabHttp.gd
+++ addons/godot-playfab/PlayFabHttp.gd
@@ -19,6 +19,8 @@ var _http: HTTPRequest
 var _request_in_progress = false
 var _title_id: String
 var _base_uri = "playfabapi.com"
+var _response_compression_enabled = true	# Whether to use response compression (gzip). If false, will send no `Accept-Encoding` header. If true, An `Accept-Encoding: gzip` header will be sent, and responses decoded with gzip.
+var _response_compression_max_output_bytes = -1 # -1 is unlimited, but this could be very large! If you change this, be aware there is no error handling implemented to catch if the output size is too small! See https://docs.godotengine.org/en/3.5/classes/class_poolbytearray.html#class-poolbytearray-method-decompress-dynamic
 
 
 func _ready():
@@ -45,7 +47,14 @@ func _get_api_url() -> String:
 func _http_request(request_method: int, body: Dictionary, path: String, callback: FuncRef, additional_headers: Dictionary = {}):
 	var json = JSON.print(body)
 	#print_debug(JSON.print(body, "\t"))
-	var headers = ["Content-Type: application/json", "Content-Length: " + str(json.length())]
+	var headers = [
+		"Content-Type: application/json",
+		"Content-Length: " + str(json.length()),
+	]
+
+	if _response_compression_enabled:
+		headers.append("Accept-Encoding: gzip")
+
 	headers.append_array(_dict_to_header_array(additional_headers))
 
 	while (_request_in_progress):
@@ -66,7 +75,9 @@ func _http_request(request_method: int, body: Dictionary, path: String, callback
 	var response_body = args[3]
 	_request_in_progress = false
 
-	var response_body_string = response_body.get_string_from_utf8()
+	var response_body_decompressed = response_body.decompress_dynamic(_response_compression_max_output_bytes, File.COMPRESSION_GZIP)
+
+	var response_body_string = response_body_decompressed.get_string_from_utf8()
 	var json_parse_result = JSON.parse(response_body_string)
 	#print_debug("JSON Parse result: %s" % JSON.print(json_parse_result.result, "\t"))
 
