743ddf58b380e8c66d7ac5ff80413a4c5e892908
Johannes Ebner <Structed@users.noreply.github.com>
2023-01-20 10:31:43 +0100

    Removed typehint that was inaccurate

--- addons/godot-playfab/Models/AbstractJsonSerializableCollection.gd
+++ addons/godot-playfab/Models/AbstractJsonSerializableCollection.gd
@@ -36,10 +36,11 @@ func clear():
 ## MASSIVE HACK 😬
 # Casts this collection and sub-objects to an Array of Dictionaries, recursively.
 # The method name is only `to_dict` to keep the API compatible with `JsonSerializable`
-# The return value is an ARRAY! The type hint is also just there to keep API compatible
-func to_dict() -> Dictionary:
+# The return value is an ARRAY!
+# @returns Array: An array of all the Dictionaries, marshaled from objects inheriting from `JsonSerializable`
+func to_dict():
 	var index = 0
-	var dict = []	# Actually, Array of items! It'S called dict because
+	var dict = []	# Actually, Array of items! It's called dict to keep rthe API compatible with JsonSerializable
 	for item in _Items:
 		dict.append((item as JsonSerializable).to_dict())
 		index += 1
