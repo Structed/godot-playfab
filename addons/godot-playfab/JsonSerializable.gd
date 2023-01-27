6d626eeb5376a53caa68962dd55fbb3772eea3fd
Johannes Ebner <Structed@users.noreply.github.com>
2023-01-27 19:04:12 +0100

    fix automatic, wrong upgrade replacement

--- addons/godot-playfab/JsonSerializable.gd
+++ addons/godot-playfab/JsonSerializable.gd
@@ -21,7 +21,7 @@ func to_dict() -> Dictionary:
 	# Skipping the first 3 items because they are metadata we do not need
 	for i in range(3, props.size()):
 		var prop = props[i]
-		var name = prop["name"] # The name of the property checked the object. WIll be used to access its's value
+		var name = prop["name"] # The name of the property on the object. Will be used to access its's value
 		var type = prop["type"]	# The godot built-in type (Array, Object etc)
 
 		if (type == TYPE_OBJECT):
