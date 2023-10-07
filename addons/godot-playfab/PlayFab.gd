d20807d42e811ed56944a00170695c611ec147d4
Johannes Ebner <Structed@users.noreply.github.com>
2023-10-07 18:14:00 +0200

    Add anonymous login method

--- addons/godot-playfab/PlayFab.gd
+++ addons/godot-playfab/PlayFab.gd
@@ -73,6 +73,14 @@ func login_with_custom_id(custom_id: String, create_user: bool, info_request_par
 
 	var result = _post(request_params, "/Client/LoginWithCustomID", Callable(self, "_on_login_with_email"))
 
+# Anonymous login with a GUID as username
+func login_anonymous():
+	var combined_info_request_params = GetPlayerCombinedInfoRequestParams.new()
+	combined_info_request_params.show_all()
+	var player_profile_view_constraints = PlayerProfileViewConstraints.new()
+	combined_info_request_params.ProfileConstraints = player_profile_view_constraints
+
+	PlayFabManager.client.login_with_custom_id(UUID.v4(), true, combined_info_request_params)
 
 func _on_register_email_password(result: Dictionary):
 	var register_result = RegisterPlayFabUserResult.new()
