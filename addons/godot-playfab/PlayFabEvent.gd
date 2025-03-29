9d753ed9f57f1c887deea8d432601d04d9f2338b
cempa96 <132065695+cempa96@users.noreply.github.com>
2025-03-29 19:57:50 +0100

    Update PlayFabEvent.gd

--- addons/godot-playfab/PlayFabEvent.gd
+++ addons/godot-playfab/PlayFabEvent.gd
@@ -153,7 +153,7 @@ func _flush_batches_on_batch_size_met():
 # @Visibility: Private
 func _flush_playstream_event_batch():
 	if playstream_event_batch.size() < 1:
-		print_debug("No telemetry events to flush")
+		print_debug("No playstream events to flush")
 		return
 
 	var request = WriteEventsRequest.new()
@@ -167,7 +167,7 @@ func _flush_playstream_event_batch():
 # @Visibility: Private
 func _flush_telemetry_event_batch():
 	if telemetry_event_batch.size() < 1:
-		print_debug("No playstream events to flush")
+		print_debug("No telemetry events to flush")
 		return
 
 	var request = WriteEventsRequest.new()
