847b362ee22c41283e641a34bc0bf24c9ac0b95d
Johannes Ebner <Structed@users.noreply.github.com>
2025-09-21 02:17:50 +0200

    Added searchItems request to catalog

---
+++ addons/godot-playfab/Models/SearchItemsRequest.gd
@@ -0,0 +1,47 @@
+extends JsonSerializable
+class_name SearchItemsRequest
+
+## Number of items to retrieve. This value is optional. Maximum page size is 50. Default value is 10.
+var Count: int
+
+## An opaque token used to retrieve the next page of items, if any are available.
+var ContinuationToken: String = ""
+
+## The optional custom tags associated with the request (e.g. build number, external trace identifiers, etc.).
+var CustomTags: Dictionary[String, Variant] = {}
+
+## The entity to perform this action on.
+var Entity: EntityKey
+
+## An OData filter used to refine the search query (For example: "type eq 'ugc'"). More info about Filter Complexity limits can be found here: https://learn.microsoft.com/en-us/gaming/playfab/features/economy-v2/catalog/search#limits
+var Filter: String = ""
+
+## The locale to be returned in the result.
+var Language: String = ""
+
+## An OData orderBy used to order the results of the search query. For example: "rating/average asc"
+var OrderBy: String = ""
+
+## The text to search for.
+var Search: String = ""
+
+## An OData select query option used to augment the search results. If not defined, the default search result metadata will be returned.
+var Select: String = ""
+
+## The store to restrict the search request to.
+var Store: StoreReference
+
+
+func _get_type_for_property(property_name: String) -> String:
+	match property_name:
+#		"<PROPERTY NAME>":
+#			return "<PROPERTY TYPE>"
+		"Entity":
+			return "EntityKey"
+		"Store":
+			return "StoreReference"
+		_:
+			pass
+
+	push_error("Could not find mapping for property: " + property_name)
+	return super._get_type_for_property(property_name)
--- addons/godot-playfab/PlayFabCatalog.gd
+++ addons/godot-playfab/PlayFabCatalog.gd
@@ -11,3 +11,15 @@ class_name PlayFabCatalog
 ## @tutorial(Request Documentation): https://docs.microsoft.com/gaming/playfab/features/economy/catalog/get-items
 func get_items(request_data: GetItemsRequest = GetItemsRequest.new(), callback: Callable = func(): pass):
 	_post_with_entity_auth(request_data, "/Catalog/GetItems", callback)
+
+## Executes a search against the public catalog using the provided search parameters
+## and returns a set of paginated results.
+## SearchItems uses a cache of the catalog with item updates taking up to a few minutes to propagate.
+## You should use the GetItem API for when trying to immediately get recent item updates.
+## Callback receives a [SearchItemsResponse] of items which can be paginated with a continuation token.
+## More information about the Search API can be found here:
+## @tutorial(Search API): https://learn.microsoft.com/en-us/gaming/playfab/features/economy-v2/catalog/search
+## @tutorial(Request Documentation): https://docs.microsoft.com/gaming/playfab/features/economy/catalog/get-items
+func search_items(request_data: SearchItemsRequest = SearchItemsRequest.new(), callback: Callable = func(): pass):
+	_post_with_entity_auth(request_data, "/Catalog/SearchItems", callback)
+	
