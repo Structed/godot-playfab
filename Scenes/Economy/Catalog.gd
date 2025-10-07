e19aeaa60dd9c8c9fb88049a578850151de243b2
Johannes Ebner <Structed@users.noreply.github.com>
2025-10-07 22:29:34 +0200

    Use cached catalog

--- Scenes/Economy/Catalog.gd
+++ Scenes/Economy/Catalog.gd
@@ -2,10 +2,14 @@ extends Control
 
 @onready var card_scene: PackedScene = preload("res://Scenes/Widgets/ItemCard.tscn")
 
+var _catalog: Dictionary[String, CatalogItem] = {}          # item_id -> ShopItem
+
 func _ready() -> void:
 	%LoadingIndicator.show()
 	PlayFabManager.catalog.search_complete.connect(_on_search_complete)
-	PlayFabManager.catalog.fetch_catalog()
+	_catalog = PlayFabManager.catalog.get_catalog()
+	if _catalog.size() > 0:
+		_on_search_complete(_catalog)
 
 	var catalog: PlayFabCatalog = PlayFabCatalog.new()
 	add_child(catalog)
@@ -14,7 +18,8 @@ func _ready() -> void:
 
 
 func _on_search_complete(result: Dictionary[String, CatalogItem]) -> void:
-	for key in result.keys():
+	_catalog = PlayFabManager.catalog.get_catalog()
+	for key in _catalog:
 		var catalog_item: CatalogItem = result[key]
 		var card: ItemCard = card_scene.instantiate()
 		card.reset(catalog_item)
