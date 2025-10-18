f8f9d88401c0a2c2d3245fe76b09a6fffcccd4ef
Johannes Ebner <Structed@users.noreply.github.com>
2025-10-18 11:22:01 +0200

    Initial commit of backend project

---
+++ backend/GodotPlayFab.Backend.Functions/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Build().Run();
